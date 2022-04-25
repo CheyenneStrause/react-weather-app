8b9d6aa42116b046ac6c827d1d27e5ed9a6515d6
CheyenneStrause <90160844+CheyenneStrause@users.noreply.github.com>
2022-04-24 19:30:23 -0700

    Created Weather forecast

---
+++ WeatherForecast.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast row">
+        {forecast.map(function (day, index) {
+          if (index < 5) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastPreview data={day} />
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    );
+  } else {
+    let apiKey = "c0f08f9c6e0b4b6c11b3856b0ab91321";
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(url).then(handleForecastResponse);
+
+    return null;
+  }
+}
